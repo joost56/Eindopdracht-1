3c086139dd9f007a7f3a8005854ad3ef3082b03f
joost56 <60775638+joost56@users.noreply.github.com>
2022-10-10 13:44:48 +0200

    set up docker

---
+++ src/main/java/nl/hu/eindopdracht1/domain/service/TaskService.java
@@ -0,0 +1,13 @@
+package nl.hu.eindopdracht1.domain.service;
+
+import lombok.RequiredArgsConstructor;
+import nl.hu.eindopdracht1.data.entity.Task;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class TaskService {
+}
