89077a45769e89ab69c65c71393a834c4af945df
Tygo-Steenbergen <99648140+Tygo-Steenbergen@users.noreply.github.com>
2022-12-08 10:42:26 +0100

    fix assignUser v2

--- Eindopdracht-1-taskservice/src/main/java/nl/hu/eindopdracht1/domain/service/HttpRequestService.java
+++ Eindopdracht-1-taskservice/src/main/java/nl/hu/eindopdracht1/domain/service/HttpRequestService.java
@@ -15,16 +15,13 @@ import javax.transaction.Transactional;
 public class HttpRequestService {
     private final RestTemplate restTemplate = new RestTemplate();
 
-    @Autowired
-    private ConfigUri configUri;
-
     public ResponseEntity<String> getUserById(String uri, String userId) {
         String url = uri + userId;
         return this.restTemplate.getForEntity(url, String.class);
     }
 
     public boolean userExists (String username) {
-        return Boolean.parseBoolean(getUserById(configUri.getUri(), username).getBody());
+        return Boolean.parseBoolean(getUserById("http://load-balancer-eindopdracht-1-199010695.us-east-1.elb.amazonaws.com/users/", username).getBody());
     }
 
 }
